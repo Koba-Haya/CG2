357daf606906d98f64dc9aaba448dc96594f10ce
Koba-Haya <k024g0038@g.neec.ac.jp>
2025-10-24 23:19:14 +0900

    01_01で提出してしまったものを01_02として訂正

--- DirectXGame/engine/base/TextureUtils.cpp
+++ DirectXGame/engine/base/TextureUtils.cpp
@@ -28,7 +28,7 @@ std::string ConvertString(const std::wstring &str) {
   auto sizeNeeded =
       WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()),
                           NULL, 0, NULL, NULL);
-  if (sizeNeeded == 0) {
+  if (sizeNeeded == 0) { 
     return std::string();
   }
   std::string result(sizeNeeded, 0);
