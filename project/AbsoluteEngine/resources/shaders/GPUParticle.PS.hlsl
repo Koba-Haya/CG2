0567926f14e609d7a6e73539946646f9b7e87eb3
Koba-Haya <haya2005hphp@gmail.com>
2026-05-20 16:02:20 +0900

    パーティクルの更新とつかいまわし完了

--- project/AbsoluteEngine/resources/shaders/GPUParticle.PS.hlsl
+++ project/AbsoluteEngine/resources/shaders/GPUParticle.PS.hlsl
@@ -14,9 +14,15 @@ struct PixelShaderOutput {
 PixelShaderOutput main(PixelShaderInput input) {
     PixelShaderOutput output;
     float32_t4 textureColor = gTexture.Sample(gSampler, input.texcoord);
-    output.color = textureColor * input.color;
-    // 透明度が0なら描画をスキップ
-    if (output.color.a == 0.0f) {
+    
+    // テクスチャのRチャネル（輝度）をアルファ値として適用
+    float32_t alpha = textureColor.r * input.color.a;
+    
+    // 出力色を決定（テクスチャのRGBと入力色を乗算、アルファには上記で求めた値を使用）
+    output.color = float32_t4(textureColor.rgb * input.color.rgb, alpha);
+    
+    // アルファがほぼ0ならピクセルを破棄
+    if (output.color.a <= 0.005f) {
         discard;
     }
     return output;
